61d40571d06554bd5984d456b355ba533e645db1
pocopota <pocopota.program@gmail.com>
2025-06-23 22:19:51 +0900

    parser開発初期

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import ast from "../datas/gen_sample_mim.json";
-import generator from "./generator";
+// generator開発
 
-const html = generator(ast);
+// import ast from "../datas/gen_sample_mim.json";
+// import generator from "./generator";
+
+// const html = generator(ast);
 
 // ファイルへ書き込み
-import fs from "fs";
-fs.writeFile("./datas/gen_sample.html", html, (err: NodeJS.ErrnoException | null) => {
-  if (err) throw err;
-  console.log('正常に書き込みが完了しました');
-});
+// import fs from "fs";
+// fs.writeFile("./datas/gen_sample.html", html, (err: NodeJS.ErrnoException | null) => {
+//   if (err) throw err;
+//   console.log('正常に書き込みが完了しました');
+// });
+
+import parser from "./parser";
+import {readFileSync} from "fs";
+import path from "path";
+
+const filePath = path.join(__dirname, "..", "datas", "prs_sample.md");
+const markdown = readFileSync(filePath, "utf-8");
+console.log(parser(markdown));
---
+++ src/parser.ts
@@ -0,0 +1,7 @@
+import { Token } from "./modules/token";
+
+const parser = (markdown: string) => {
+  return markdown;
+}
+
+export default parser;
