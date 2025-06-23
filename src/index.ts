2222a9ba525e2430f80cc089361af8fedb460924
pocopota <pocopota.program@gmail.com>
2025-06-23 16:24:08 +0900

    サンプルファイル名変更

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import ast from "../datas/sample_mim.json";
+import ast from "../datas/gen_sample_mim.json";
 import generator from "./generator";
 
 const html = generator(ast);
 
 // ファイルへ書き込み
 import fs from "fs";
-fs.writeFile("./datas/sample.html", html, (err: NodeJS.ErrnoException | null) => {
+fs.writeFile("./datas/gen_sample.html", html, (err: NodeJS.ErrnoException | null) => {
   if (err) throw err;
   console.log('正常に書き込みが完了しました');
 });
